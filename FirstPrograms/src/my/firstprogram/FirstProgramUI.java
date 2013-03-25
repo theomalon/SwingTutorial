8d9fc60f460cba9071320a0f91ca9fbddce1f03c
Chris Malon <theomalon@gmail.com>
2013-03-24 21:27:35 -0400

    Added a Quit Button to window

--- FirstPrograms/src/my/firstprogram/FirstProgramUI.java
+++ FirstPrograms/src/my/firstprogram/FirstProgramUI.java
@@ -5,17 +5,35 @@ package my.firstprogram;
     import java.awt.event.ActionListener;
     import javax.swing.JButton;
     import javax.swing.JFrame;
+    import javax.swing.JPanel;
     import javax.swing.SwingUtilities;
 
 public class FirstProgramUI extends JFrame{
     
     public FirstProgramUI(){
-       setTitle("Simple example");
+       initUI();
+    }
+    
+    public final void initUI(){
+       JPanel panel = new JPanel();
+       getContentPane().add(panel);
+
+       panel.setLayout(null);
+
+       JButton quitButton = new JButton("Quit");
+       quitButton.setBounds(50, 60, 80, 30);
+       quitButton.addActionListener(new ActionListener() {
+           public void actionPerformed(ActionEvent event) {
+               System.exit(0);
+          }
+       });
+
+       panel.add(quitButton); 
+       setTitle("Quit Button");
        setSize(300, 200);
        setLocationRelativeTo(null);
        setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
